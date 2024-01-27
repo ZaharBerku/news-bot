3354c271f897ca81b3eef2ad327e6e152d065136
ZaharBerku <zaharberku@gmail.com>
2024-01-27 22:57:08 +0200

    change order markdow

--- helpers/detectMarkdownType.js
+++ helpers/detectMarkdownType.js
@@ -1,14 +1,14 @@
 function detectMarkdownType(text) {
+  if (/<\/?[a-z][\s\S]*>/i.test(text)) {
+    return "html";
+  }
+  
   if (/#+\s.*|(\*\*?|__?|~~).+?\1|\[.*?\]\(.*?\)/.test(text)) {
     return "md";
   }
 
   if (/\\[*_()[\]~`>#+-=|{}.!]/.test(text)) {
     return "md2";
-  }
-
-  if (/<\/?[a-z][\s\S]*>/i.test(text)) {
-    return "html";
   }
 
   return "md";
